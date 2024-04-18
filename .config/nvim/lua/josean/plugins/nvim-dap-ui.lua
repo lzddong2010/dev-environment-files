cbdb674ade120d3f0b9c0f06241d6413a0b4b6ed
lzddong2010 <672625217@qq.com>
2024-04-19 00:33:40 +0800

    [Fix] add nvim-nio

--- .config/nvim/lua/josean/plugins/nvim-dap-ui.lua
+++ .config/nvim/lua/josean/plugins/nvim-dap-ui.lua
@@ -1,17 +1,30 @@
 return {
   "rcarriga/nvim-dap-ui",
-  dependencies = "mfussenegger/nvim-dap",
+  dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
   config = function()
-    local dap = require("dap")
-    local dapui = require("dapui")
-    dapui.setup()
-    dap.listeners.after.event_initialized["dap_config"] = function()
+    -- local dap = require("dap")
+    -- local dapui = require("dapui")
+    -- dapui.setup()
+    -- dap.listeners.after.event_initialized["dap_config"] = function()
+    --   dapui.open()
+    -- end
+    -- dap.listeners.before.event_terminated["dap_config"] = function()
+    --   dapui.close()
+    -- end
+    -- dap.listeners.before.event_exited["dap_config"] = function()
+    --   dapui.close()
+    -- end
+    local dap, dapui = require("dap"), require("dapui")
+    dap.listeners.before.attach.dapui_config = function()
       dapui.open()
     end
-    dap.listeners.before.event_terminated["dap_config"] = function()
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
       dapui.close()
     end
-    dap.listeners.before.event_exited["dap_config"] = function()
+    dap.listeners.before.event_exited.dapui_config = function()
       dapui.close()
     end
   end,
